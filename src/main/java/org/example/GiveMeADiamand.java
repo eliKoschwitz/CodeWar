8d86e9f2c611a1de27d8d6ecd9d5c395ff81cdca
elikoschwitz <e.koschwitz@googmail.com>
2022-11-25 09:32:04 +0100

    Change some logic now the diamond is given in one string

--- src/main/java/org/example/GiveMeADiamand.java
+++ src/main/java/org/example/GiveMeADiamand.java
@@ -9,20 +9,21 @@ public class GiveMeADiamand {
     public static String print (int n) {
         int input = n;
         String dia = "";
+        String sDia = "";
         int spaces = 1;
         spaces = input / 2;
         int astrix = 1;
         boolean flag = true;
+        String sSpaces = "";
 
         if (n % 2 == 0) {
             System.out.println("not Working!");
             return dia;
         } else {
             for (int i = 0; i < input; i++) {
-                dia = repeat(spaces, " "); // here everything is overwritten
-                dia = dia + repeat(astrix,"*");
-                dia = dia + "\r";
-                System.out.print(dia);
+                sSpaces = repeat(spaces, " ");
+                dia = sSpaces + repeat(astrix,"*");
+                sDia = sDia + dia + "\n";
 
                 if(astrix < input && flag) {
                     astrix = astrix + 2;
@@ -34,13 +35,13 @@ public class GiveMeADiamand {
                 }
             }
         }
-        return dia;
+        return sDia;
     }
 
     public static void main(String[] args) {
-        int input =11;
+        int input =5;
         print(input);
-        //System.out.println(print(input));
+        System.out.println(print(input));
     }
 }
 
